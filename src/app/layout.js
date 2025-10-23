b64b2e881087370e5499cf2ddcaa338b9faba598
Cheng <jxcheng@bu.edu>
2025-10-23 09:53:46 -0400

    Fix Indexing

--- src/app/layout.js
+++ src/app/layout.js
@@ -69,10 +69,11 @@ export default function RootLayout({ children }) {
       <head>
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"></link>
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"></link>
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"></link>
-        <link rel="manifest" href="favicon/site.webmanifest"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="canonical" href="https://playtogl.com" />
 
         <script
           dangerouslySetInnerHTML={{
