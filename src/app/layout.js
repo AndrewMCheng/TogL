160790dcb5deb889945ae04ddf7a54d50fb2616e
Cheng <jxcheng@bu.edu>
2025-10-26 18:16:37 -0400

    Wechat Accessibility

--- src/app/layout.js
+++ src/app/layout.js
@@ -109,7 +109,7 @@ export default function RootLayout({ children }) {
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"
         />
         <meta
           name="google-site-verification"
